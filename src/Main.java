ad48971cb398d08c7a5504ff43c70a96cd9b72b6
Gogle Öztaş <demiroztas06@hotmail.com>
2022-10-10 14:30:28 +0300

    finaş

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Sayı giriniz");
+        int value = input.nextInt();
+        int total = 0;
+
+
+        for(int i=1; i<value; i++)
+        {
+            if(value%i==0)
+            {
+                total+=i;
+            }
+        }
+
+       if(value==total)
+       {
+           System.out.println( value+" Mükemmel sayıdır");
+       }
+       else {
+           System.out.println( value+" Mükemmel sayı değildir.");
+       }
+
+
     }
 }
